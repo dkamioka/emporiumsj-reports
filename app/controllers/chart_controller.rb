736b34e6581b441d37bad2705dc22fd0ec3d728c
Diogo Kamioka <farofitus@gmail.com>
2011-06-13 18:18:35 -0300

    Funcionando o chart

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -1,13 +1,14 @@
 class ChartController < ApplicationController
  def index
    todas_vendas = Venda.all
-   @chart = Hash.new(0)
+   @custos = Hash.new(0)
+   @vendas = Hash.new(0)
+   @lucros = Hash.new(0)
     todas_vendas.each do |v|
-     data = v.data_geracao
-     @chart[data].nil? ? @chart[data] = Array.new(0) : 
-     @chart[data][0] += v.custo
-     @chart[data][1] += v.venda
-     @chart[data][2] += v.lucro
+     data = v.data_geracao.to_s
+     @custos[data] += v.custo
+     @vendas[data] += v.venda
+     @lucros[data] += v.lucro
    end
  end
 end
