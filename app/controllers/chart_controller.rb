5382eebf638a273f9ec72983d8703415cdca4622
Diogo Kamioka <farofitus@gmail.com>
2011-06-14 02:24:42 -0300

    chart funcionando!

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -13,9 +13,30 @@ class ChartController < ApplicationController
  end
 
  def show
+    todas_vendas = DataMapper.repository.adapter.select('select data_geracao data, sum(custo) custo, sum(venda) venda, sum(lucro) lucro from vendas group by data')
+    @datas = Array.new(0)
+    @custos = Array.new(0)
+    @vendas = Array.new(0)
+    @lucros = Array.new(0)
+    todas_vendas.each do |v|
+      @datas << v.data.strftime('%d/%m/%Y')
+      @custos << v.custo
+      @vendas << v.venda
+      @lucros << v.lucro
+    end
    @h = LazyHighCharts::HighChart.new('graph') do |f|
-         f.series(:name=>'John', :data=>[3, 20, 3, 5, 4, 10, 12 ,3, 5,6,7,7,80,9,9])
-             f.series(:name=>'Jane', :data=> [1, 3, 4, 3, 3, 5, 4,-46,7,8,8,9,9,0,0,9] )
-        end
+         f.series(:name=>'Custos', :data=>@custos)
+         f.series(:name=>'Vendas', :data=>@vendas)
+         f.series(:name=>'Lucros', :data=>@lucros)
+         f.x_axis(:categories => @datas )
+         f.title( :text => "Vendas de #{@datas.first} a #{@datas.last}" )
+   end
+ end
+
+ def showa
+     @h = LazyHighCharts::HighChart.new('graph') do |f|
+    f.series(:name=>'John', :data=>[3, 20, 3, 5, 4, 10, 12 ,3, 5,6,7,7,80,9,9])
+    f.series(:name=>'Jane', :data=> [1, 3, 4, 3, 3, 5, 4,-46,7,8,8,9,9,0,0,9] )
+  end
  end
 end
